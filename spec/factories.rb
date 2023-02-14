2dd0073b7c6c7da63577ff2ec2a81d5c9827d70e
Kirk Wang <k3wang@gmail.com>
2023-02-13 20:52:19 -0800

    Generate resource Deck
    Also made `#show` and `#index` work a little better by authenticating
    the user so the user only sees their own decks.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
+  factory :deck do
+    name { "MyString" }
+    user_id { 1 }
+  end
+
   factory(:user) do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
